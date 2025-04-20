8152f834ab6377f55ec68b6bdd7efd398ee016d2
Shahriar <arfin17310@gmail.com>
2025-04-21 02:19:08 +0600

    feat(db): integrated convex

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,12 @@ import { Slot } from "expo-router";
 import { useFonts } from "expo-font";
 import { Sora_700Bold } from "@expo-google-fonts/sora";
 import { Mulish_500Medium, Mulish_600SemiBold, Mulish_700Bold } from "@expo-google-fonts/mulish";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+
+// Initialize Convex client
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+   unsavedChangesWarning: false
+});
 
 export { ErrorBoundary } from "expo-router";
 
@@ -17,5 +23,9 @@ export default function RootLayout() {
 
    if (!fontsLoaded) return null;
 
-   return <Slot />;
+   return (
+      <ConvexProvider client={convex}>
+         <Slot />
+      </ConvexProvider>
+   );
 }
